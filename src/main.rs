c774b11d0cc92096e1afdb05ef51c0b6f57d7cff
huhn511 <sehe89@gmail.com>
2020-06-17 18:29:00 +0200

    :sparkles: add trytes to trits example

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use bee_ternary::{T1B1Buf, TryteBuf};
+
+
+fn main() {
+    // String with Trytes [alphabet: A-Z and 9]
+    const HELLO__TRYTES_STRING: &str = "HELLOWORLD9";
+
+    // Convert Trytesstring to trits
+    let hello_trits = TryteBuf::try_from_str(HELLO__TRYTES_STRING)
+        .unwrap()
+        .as_trits()
+        .encode::<T1B1Buf>();
+
+    // Print trits
+    println!("'Hello World!' in trits: {}", hello_trits);
+
+}
